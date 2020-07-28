217f4c61c65e442a9ca15c2f97e77d6e1fa36b21
zymryte <zymryteeek@gmail.com>
2020-07-28 14:10:57 +0200

    some changes

--- app_one/models.py
+++ app_one/models.py
@@ -57,24 +57,6 @@ class Movie(models.Model):
     objects = MovieManager()
 
 
-    
-class MovieShowTime(models.Model):
-    date = models.DateField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    showtime = models.ManyToManyField(Movie, related_name='showtime')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now= True)
-        
-    
-class Review(models.Model):
-    rating = models.IntegerField(default=1, validators=[MaxValueValidator(5), MinValueValidator(1)])
-    review = models.ForeignKey(Movie, related_name='has_reviews', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    
 class CinoRoom(models.Model):
     ROOM_CHOICES = [
         ('A1', 'A1'),
@@ -89,22 +71,35 @@ class CinoRoom(models.Model):
         
     ]
     room = models.CharField(max_length=2, choices = ROOM_CHOICES, default='A1')
-    movie = models.ForeignKey(Movie, related_name='movie_room', on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     
+class MovieShowTime(models.Model):
+    date = models.DateField()
+    movie = models.ForeignKey(Movie, related_name='movie', on_delete = models.CASCADE)
+    room = models.ForeignKey(CinoRoom, related_name='movie_room', on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now= True)
+           
+    
 class Booking(models.Model):
     tickets = models.IntegerField()
     price = models.DecimalField(max_digits=4, decimal_places=2)
-    cino_room = models.ForeignKey(CinoRoom, related_name='rooms', on_delete = models.CASCADE)
+    booking = models.ForeignKey(MovieShowTime, related_name='showtime', on_delete = models.CASCADE)
     buyer = models.ForeignKey(User, related_name='has_tickets', on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, related_name='movie_tickets', on_delete= models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)  
     
-
-
+    
+class Review(models.Model):
+    rating = models.IntegerField(default=1, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    review = models.ForeignKey(Movie, related_name='has_reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
 class CommentManager(models.Manager):
     def comm_validator(self, postData):
         errors = {}     
@@ -115,7 +110,7 @@ class CommentManager(models.Manager):
 class Comments(models.Model):
     content = models.TextField()
     posted_by = models.ForeignKey(User, related_name='has_comments', on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, related_name='movie', on_delete = models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='has_comments', on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = CommentManager()
